a050e555dae6ae7ca617629cc3404eaf3a98b970
Mahialam Rahat <mahialam408@gmail.com>
2025-11-13 16:56:03 +0600

    Netlify Ready

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
     baseURL: 'https://utility-bills-managment-server.vercel.app'
     // baseURL: 'http://localhost:3000'
+    //Noo need
 })
 
 const useAxios = () => {
